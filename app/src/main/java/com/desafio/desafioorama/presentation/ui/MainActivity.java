7d2e6ac2ad1f8465b4a621db79955f9fcb0eadd2
Tilau <stanis.campregher@sinergiastudios.com>
2021-05-31 14:37:03 -0300

    initial commit

---
+++ app/src/main/java/com/desafio/desafioorama/presentation/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.desafio.desafioorama.presentation.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.desafio.desafioorama.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
